158fc2f1ed4034c602e5adbfa47b31512308b00a
Edgar Ivan Garcia <edgar.ivan.garcia@ibm.com>
2022-01-19 06:39:36 -0600

    Update carbon style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { 
+  Header, 
+  HeaderName,
+  SideNav,
+  SideNavMenu,
+  SideNavMenuItem,
+  SideNavItems,
+  SideNavLink,
+  Accordion,
+  AccordionItem
+} from 'carbon-components-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#" prefix="IBM">
+          [Power Forum]
+        </HeaderName>
+      </Header>
+
+      <SideNav      
+        isFixedNav expanded={true}
+      isChildOfHeader={false}
+      aria-label="Side navigation">
+        <SideNavItems>
+
+          <SideNavMenu title="Fallas Comunes">
+            <SideNavMenuItem href="javascript:void(0)">Glosario SRC'S</SideNavMenuItem>
+            <SideNavMenuItem href="javascript:void(0)">WorkAround's</SideNavMenuItem>
+            <SideNavMenuItem href="javascript:void(0)">Aportes de Ingenieria</SideNavMenuItem>
+          </SideNavMenu>
+
+          <SideNavMenu title="Sistemas Golden" isActive={true}>
+            <SideNavMenuItem href="/puto">Denali/FleetWood</SideNavMenuItem>
+            <SideNavMenuItem aria-current="page" href="javascript:void(0)">Tuleta/ZZ/Zeppellin</SideNavMenuItem>
+            <SideNavMenuItem href="javascript:void(0)">Partes Golden</SideNavMenuItem>
+          </SideNavMenu>
+
+          <SideNavMenu title="Manuals">
+            <SideNavMenuItem href="javascript:void(0)">ZZ2/ZZ4/ZZ Tower</SideNavMenuItem>
+            <SideNavMenuItem href="javascript:void(0)">Open Power</SideNavMenuItem>
+            <SideNavMenuItem href="javascript:void(0)">Fleetwood/Denali/Brazo</SideNavMenuItem>
+          </SideNavMenu>
+
+          <SideNavLink href="javascript:void(0)">
+              Avisos
+          </SideNavLink>
+
+          <SideNavLink href="javascript:void(0)">
+              PCM'S
+          </SideNavLink>
+
+        </SideNavItems>
+      </SideNav>
+
+      <div className='landing-page-content'>
+      
+      <div className='box'>
+        <h1>People</h1>
+        <h1>Choice</h1>
+        <h1>Awards</h1>
+      </div>
+
+      <div className='box'>
+        <h1>Announces</h1>
+      </div>
+
+      <div className='box'>
+        <h1>Power Systems</h1>
+      </div>
+
+      </div>
     </div>
   );
 }
